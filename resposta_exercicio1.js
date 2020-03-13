7388af0e23faa7e2fe8fe1ae78490be84d045062
Rafael <rafael_egqsociesc@hotmail.com>
2020-03-13 18:44:28 -0300

    questionario e resolução do exercicio 1

---
+++ resposta_exercicio1.js
@@ -0,0 +1,27 @@
+var myvar;
+
+myvar = 10;
+
+var soma = 15 + 8;
+
+soma = soma++;
+
+soma *= 3;
+
+// Qual é o valor da variável `soma` até aqui? R: 72
+
+var souninja = true;
+
+var comida  = ['arroz', 'feijão', 'ovo'];
+
+comida[1];
+
+soma === myvar;
+
+myvar <= soma;
+
+function divisao(a,b){
+    return a / b;
+  }
+
+divisao(10, 2);
